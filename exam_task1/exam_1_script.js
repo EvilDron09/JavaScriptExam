c29a86e906e293a11ab68a74b03975ef340bf560
EvilDron09 <andrej32118@gmail.com>
2026-03-10 23:53:34 +0200

    exam_1 app 1

--- exam_task1/exam_1_script.js
+++ exam_task1/exam_1_script.js
@@ -3,35 +3,51 @@ const inputText = document.getElementById('textInput');
 const listDiv = document.getElementById('boxList');
 const buttonsBlock = document.getElementsByTagName('button');
 
+
+
+
 function Text (name,value){
     this.name = name;
     this.value = value
 }
 let textList =[]
 
+
+function deleteSelectionElement (element){
+    element.addEventListener('mousedown',(e)=> {
+        if(e.target.matches('p')){
+            element.className='deleteElement'
+            element.style.background ='red';
+        }
+    })
+}
+
 // Додає об'єкти до списку
 writeValueForm.addEventListener('submit',(e) =>{
         e.preventDefault();
             const textInputS = writeValueForm.textInput.value;
-            let textObj = {textInputS};
-            localStorage.setItem('textObj', JSON.stringify(textObj));
-            const pList = document.createElement('p');
             const textInputSplit = textInputS.split('=');
             const textInputName = textInputSplit[0].trim();
             const textInputValue = textInputSplit[1].trim();
-            textList.push(new Text(textInputName, textInputValue))
-            pList.innerText = `${textInputName}=${textInputValue}`
+            let textObj = {textInputName,textInputValue};
+            localStorage.setItem('textObj', JSON.stringify(textObj));
+            const pList = document.createElement('p');
+            textList.push(new Text(textInputName, textInputValue));
+            pList.innerText = `${textInputName}=${textInputValue}`;
+            deleteSelectionElement(pList);
             listDiv.appendChild(pList);
             inputText.value = ''
 })
+
 // Сортування об'єктів за ім'ям в порядку зростання
 buttonsBlock[0].addEventListener('click',(e) =>{
     e.preventDefault();
     let textListNameSort= textList.sort((text1, text2) => text1.name.localeCompare(text2.name));
-    listDiv.innerHTML = '';
+    listDiv.innerText = '';
     for (const textListNameSortElement of textListNameSort) {
         const sortName = document.createElement('p');
         sortName.innerText = `${textListNameSortElement.name}=${textListNameSortElement.value}`;
+        deleteSelectionElement(sortName);
         listDiv.appendChild(sortName);
     }
 })
@@ -39,15 +55,24 @@ buttonsBlock[0].addEventListener('click',(e) =>{
 buttonsBlock[1].addEventListener('click',(e)=>{
     e.preventDefault();
     let textListValueSort = textList.sort((text1, text2) => text1.value.localeCompare(text2.value));
-    listDiv.innerHTML = '';
+    listDiv.innerText = '';
     for (const textListValueSortElement of textListValueSort) {
         const sortValue = document.createElement('p');
         sortValue.innerText =`${textListValueSortElement.name}=${textListValueSortElement.value}`;
+        deleteSelectionElement(sortValue);
         listDiv.appendChild(sortValue);
     }
 })
+
 // Видаляє всі об'єкти
 buttonsBlock[2].addEventListener('click',(e)=>{
     e.preventDefault();
-    listDiv.innerHTML=''
+    document.querySelectorAll('.deleteElement').forEach(element => {
+        const [name,value] = element.innerText.split('=').map(item => item.trim())
+        textList = textList.filter( item =>{
+            return item.name !== name && item.value !== value})
+        element.remove();
+
+    })
+
 })
